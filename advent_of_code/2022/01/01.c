b104f284983bd1a0616e38ad3324bf7624bb933f
root <root@ts2.lan>
2022-12-04 09:09:39 -0700

    day1

---
+++ advent_of_code/2022/01/01.c
@@ -0,0 +1,45 @@
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void nummax(void);
+
+int num, max1, max2, max3;
+
+int
+main(void)
+{
+        FILE *fp;
+        char buf[1024];
+
+        if ((fp = fopen("./input", "r")) == NULL) err(1, "fopen");
+        while (fgets(buf, sizeof(buf), fp) != NULL) {
+                if (buf[0] == '\n') {
+                        (void)nummax();
+                        num = 0;
+                        continue;
+                }
+
+                num += atoi(buf);
+        }
+        fclose(fp); fp = NULL;
+        (void)nummax();
+        (void)printf("%d\n", max1 + max2 + max3);
+
+        return 0;
+}
+
+void
+nummax(void)
+{
+        if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+        } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+        } else if (num > max3) {
+                max3 = num;
+        }
+}
